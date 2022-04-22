66c4406f9ab81e5f1cba62b08dc0a8c6bb3c7463
Arthur Rocha <artthurroccha@gmail.com>
2022-04-22 18:26:11 -0300

    Adicionar arquivos de projeto.

---
+++ app-crud-backend/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_crud_backend.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
